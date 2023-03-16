64238cb2479ada708141d38c3e984cab4601470b
Stephen CUI <junocuixuan@163.com>
2023-03-16 22:45:46 +0800

    基于测试数据的评价

--- common/function.py
+++ common/function.py
@@ -6,8 +6,8 @@ def softmax(x):
     # 完全可以用axis=1替代
     if x.ndim == 2:
         x = x.T
-        x = x - x.np.max(x, axis=0)
-        y = np.np.exp(x) / np.sum(np.exp(x), axis=0)
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
         return y.T
 
     x = x - np.max(x)
@@ -24,3 +24,15 @@ def identify_function(x):
 
 def relu(x):
     return np.maximum(0, x)
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
