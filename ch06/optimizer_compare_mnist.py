4fd3bf3e3c9edafd83321623bc23ff5585a821cd
sunyardCUI <1924375084@qq.com>
2023-03-23 14:07:49 +0800

    optimizer_compare_mnist

--- ch06/optimizer_compare_mnist.py
+++ ch06/optimizer_compare_mnist.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 sys.path.append(os.pardir)
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,33 +28,23 @@ for key in optimizers.keys():
         input_size=784, hidden_size_list=[100, 100, 100, 100],
         output_size=10
     )
-    
+
     train_loss[key] = []
-    
+
 for i in range(max_iterations):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    
+
     for key in optimizers.keys():
         grads = networks[key].gradient(x_batch, t_batch)
         optimizers[key].update(networks[key].params, grads)
-        
+
         loss = networks[key].loss(x_batch, t_batch)
         train_loss[key].append(loss)
-        
+
     if i % 100 == 0:
         print('=======' + 'iteration: ' + str(i) + '=======')
         for key in optimizers.keys():
             loss = networks[key].loss(x_batch, t_batch)
             print(key + ':' + str(loss))
-    
-markers = {"SGD": "o", "Momentum": "x", "AdaGrad": "s", "Adam": "D"}
-x = np.arange(max_iterations)
-for key in optimizers.keys():
-    plt.plot(x, smooth_curve(train_loss[key]), marker=markers[key], markevery=100, label=key)
-plt.xlabel("iterations")
-plt.ylabel("loss")
-plt.ylim(0, 1)
-plt.legend()
-plt.show()
