282344e8dfe1f8a12d40b647e8af309e23485143
Stephen CUI <junocuixuan@163.com>
2023-03-26 22:01:09 +0800

    dropout

--- ch06/batch_norm_test.py
+++ ch06/batch_norm_test.py
@@ -7,3 +7,82 @@ import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
 from common.multi_layer_net_extend import MultiLayerNetExtend
 from common.optimizer import SGD, Adam
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+# 使用少量学习数据
+
+x_train = x_train[:1000]
+t_train = t_train[:1000]
+
+max_epochs = 20
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = .01
+
+
+def __train(weight_init_std):
+    bn_network = MultiLayerNetExtend(
+        input_size=784, hidden_size_list=[100] * 5, output_size=10, weight_init_std=weight_init_std, use_batch_norm=True)
+    network = MultiLayerNetExtend(
+        input_size=784, hidden_size_list=[100] * 5, output_size=10, weight_init_std=weight_init_std)
+    optimizer = SGD(lr=learning_rate)
+    train_acc_list = []
+    bn_train_acc_list = []
+
+    iter_per_epoch = max(train_size / batch_size, 1)
+    epoch_cnt = 0
+    for i in range(100_000_000):
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+
+        for _network in (bn_network, network):
+            grads = _network.gradient(x_batch, t_batch)
+            optimizer.update(_network.params, grads)
+
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            bn_train_acc = bn_network.accuracy(x_train, t_train)
+            train_acc_list.append(train_acc)
+            bn_train_acc_list.append(bn_train_acc)
+
+            print('epoch: ' + str(epoch_cnt) + '|' +
+                  str(train_acc) + '-' + str(bn_train_acc))
+
+            epoch_cnt += 1
+            if epoch_cnt >= max_epochs:
+                break  # 直接跳出range循环
+    return train_acc_list, bn_train_acc_list
+
+
+weight_scale_list = np.around(np.logspace(0, -4, num=16), 4)
+x = np.arange(max_epochs)
+
+fig, axes = plt.subplots(2, 8, figsize=(32, 8))
+for (i, w), ax in zip(enumerate(weight_scale_list), axes.ravel()):
+    print('=========' + str(i + 1) + '/16' + '=========')
+    train_acc_list, bn_train_acc_list = __train(w)
+    ax.set_title('W=' + str(w))
+    ax.plot([1, 2], [3, 4], label='a')
+    ax.plot([1, 2], [5, 64], label='b')
+    ax.plot(x, bn_train_acc_list, markevery=2)
+    ax.plot(x, train_acc_list, '--', markevery=2)
+    ax.set_ylim(0, 1)
+    if i % 8:
+        ax.set_yticks([])
+    else:
+        ax.set_ylabel('Accuracy')
+    if i < 8:
+        ax.set_xticks([])
+    else:
+        ax.set_xlabel('Epoch')
+
+
+line, label = axes.ravel()[0].get_legend_handles_labels()
+
+fig.legend(['Batch Normalization', 'Normal(without BatchNorm)'], ncol=2, bbox_to_anchor=(
+    1, 1.05), bbox_transform=fig.transFigure, fontsize=15)
+plt.tight_layout()
+plt.show()
--- common/multi_layer_net_extend.py
+++ common/multi_layer_net_extend.py
@@ -8,7 +8,7 @@ from common.gradient import numerical_gradient
 from typing import List
 
 
-class MultiLayerExtend:
+class MultiLayerNetExtend:
     """
     拓展版的全连接多层神经网络
     具有权重衰减（weight decay），dropout, batch norm
@@ -40,11 +40,126 @@ class MultiLayerExtend:
                             str(idx)] = np.ones(hidden_size_list[idx - 1])
                 self.params['beta' +
                             str(idx)] = np.zeros(hidden_size_list[idx - 1])
-                self.layers['BatchNorm' + str(idx)] = BatchNormalization()
+                self.layers['BatchNorm' + str(idx)] = BatchNormalization(
+                    self.params['gamma' + str(idx)], self.params['beta' + str(idx)])
+            self.layers['Activation_function' +
+                        str(idx)] = activation_layer[activation]()
+            if self.use_dropout:
+                self.layers['Dropout' + str(idx)] = Dropout(dropout_ration)
+        idx = self.hidden_layer_num + 1
+        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
+                                                  self.params['b' + str(idx)])
+        self.last_layer = SoftmaxWithLoss()
 
     def __init_weights(self, weight_init_std):
-        """_summary_
+        """设定权重的初始值
 
         Args:
             weight_init_std (_type_): _description_
         """
+
+        all_size_list = [self.input_size] + \
+            self.hidden_size_list + [self.output_size]
+        for idx in range(1, len(all_size_list)):
+            scale = weight_init_std
+            if str(weight_init_std).lower() in ('relu', 'he'):
+                scale = np.sqrt(2.0 / all_size_list[idx - 1])
+            elif str(weight_init_std).lower() in ('xavier', 'sigmoid'):
+                scale = np.sqrt(1.0 / all_size_list[idx - 1])
+            self.params['W' + str(idx)] = scale * \
+                np.random.randn(all_size_list[idx - 1], all_size_list[idx])
+            self.params['b' + str(idx)] = scale * \
+                np.random.randn(all_size_list[idx])
+
+    def predict(self, x, train_flg=False):
+        """_summary_
+
+        Args:
+            x (_type_): _description_
+            train_flg (bool, optional): _description_. Defaults to False.
+        """
+        for key, layer in self.layers.items():
+            # 只对dropout和batch norm层做，这两个类的前向传播方法确实有两个参数
+            if 'Dropout' in key or 'BatchNorm' in key:
+                x = layer.forward(x, train_flg)
+            else:
+                x = layer.forward(x)
+        return x
+
+    def loss(self, x, t, train_flg=False):
+        """_summary_
+
+        Args:
+            x (_type_): _description_
+            t (_type_): _description_
+            train_flg (bool, optional): _description_. Defaults to False.
+        """
+        y = self.predict(x, train_flg)
+        weight_decay = 0
+        for idx in range(1, self.hidden_layer_num + 2):
+            W = self.params['W' + str(idx)]
+            weight_decay += .5 * self.weight_decay_lambda * np.sum(W ** 2)
+        return self.last_layer.forward(y, t) + weight_decay
+
+    def accuracy(self, X, T):
+        """_summary_
+
+        Args:
+            x (_type_): _description_
+            T (_type_): _description_
+        """
+        Y = self.predict(X, train_flg=False)
+        Y = np.argmax(Y, axis=1)
+        if T.ndim != 1:
+            T.np.argmax(T, axis=1)
+        accuracy = np.sum(Y == T) / float(X.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, X, T):
+        """_summary_
+
+        Args:
+            X (_type_): _description_
+            T (_type_): _description_
+        """
+        def loss_W(W): return self.loss(X, T, train_flg=False)
+        grads = {}
+        for idx in range(1, self.hidden_layer_num + 2):
+            grads['W' + str(idx)] = numerical_gradient(loss_W,
+                                                       self.params['W' + str(idx)])
+            grads['b' + str(idx)] = numerical_gradient(loss_W,
+                                                       self.params['b' + str(idx)])
+            if self.use_batch_norm and idx != (self.hidden_layer_num + 1):
+                grads['gamma' + str(idx)] = numerical_gradient(loss_W,
+                                                               self.params['gamma' + str(idx)])
+                grads['beta' + str(idx)] = numerical_gradient(loss_W,
+                                                              self.params['beta' + str(idx)])
+        return grads
+
+    def gradient(self, x, t):
+        """_summary_
+
+        Args:
+            x (_type_): _description_
+            t (_type_): _description_
+        """
+        self.loss(x, t, train_flg=True)
+
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        grads = {}
+        for idx in range(1, self.hidden_layer_num + 2):
+            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + \
+                self.weight_decay_lambda * self.params['W' + str(idx)]
+            grads['b' + str(idx)] = self.layers['Affine' + str(idx)].db
+            if self.use_batch_norm and idx != self.hidden_layer_num + 1:
+                grads['gamma' +
+                      str(idx)] = self.layers['BatchNorm' + str(idx)].dgamma
+                grads['beta' +
+                      str(idx)] = self.layers['BatchNorm' + str(idx)].dbeta
+        return grads
